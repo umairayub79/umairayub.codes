335b7de46def6df0bb4e202622d45e4822c16551
umairayub79 <umairayub79@gmail.com>
2019-10-21 01:25:14 +0500

    added hover effects

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
+import '../css/Navbar.css'
 export default class Navbar extends Component {
     render() {
         return (
