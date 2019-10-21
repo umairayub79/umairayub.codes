6ce18def0757587c3bfbf3ad881c0ee260cb376f
umairayub79 <umairayub79@gmail.com>
2019-10-21 20:25:00 +0500

    changed navbar item hover effects

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
 import '../css/Navbar.css'
+
 export default class Navbar extends Component {
     render() {
         return (
             <React.Fragment>
             <div className="navigation">
-            <div className="navigation-sub">
-
-              <Link to="/" className="item">Home</Link>
+              
+              <div className="navigation-sub">
+                <Link to="/" className="item">Home</Link>
+              </div>
+              <div className="navigation-sub">
               <Link to="/Projects" className="item">Projects</Link>
-    
-            </div>
+              </div>
           </div>
           </React.Fragment>
         )
