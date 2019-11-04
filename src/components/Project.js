b86b372847480091a373d41bfbddbf87d2243539
umairayub79 <umairayub79@gmail.com>
2019-11-04 11:01:29 +0500

    wrapped project in <a href> tag

--- src/components/Project.js
+++ src/components/Project.js
@@ -18,14 +18,15 @@ class Project extends Component {
         return (
             <React.Fragment>
                 <div className="project_item">
+                <a href={this.props.link} target="_blank">
                     <h4 className="project_title">{this.props.name}</h4>
                     <p className="project_description">{this.props.description}</p>
                     <p>{this.props.type}</p>
                     <p className="project_date">{this.props.date}</p>
                     <div class="projectLink">
-                        <a href={this.props.link} target="_blank">{this.props.linkText} > </a>
+                        <a href={this.props.link} target="_blank">{this.props.linkText}</a>
                     </div>
-
+                </a>
                 </div>
             </React.Fragment>
         )
