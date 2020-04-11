2ecbf1abeae3cfa6fa802646685c3de019f8012f
umairayub79 <umairayub79@gmail.com>
2020-04-11 14:43:05 +0500

    fixed title of projects section:

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,27 +1,103 @@
-import React, { Component } from 'react'
-import Project from './Project'
-import '../css/Projects.css'
+import React, { Component, Fragment } from "react";
+import Helmet from "react-helmet";
+import Project from "./Project";
+import "../css/Projects.css";
+const pageTitle = "Projects";
+
 class Projects extends Component {
-    render() {
-        return (
-            <React.Fragment>
-                <h1>Projects</h1>
-                <div className="projects">  
-                    <div className="projects_list">
-                        <Project name="AppManager" description="Manage all your apps" type="Android App" date="December 2018" link="https://ply.gl/umairayub.appmanager" linkText="Google Play"/>
-                        <Project name="BitLauncher" description="Minimalist Launcher" type="Android App" date="July 2019" link="https://ply.gl/umairayub.bitlauncher" linkText="Google Play"/> 
-                        <Project name="HTML Generator" description="Generate Html in a Click" type="Android App" date="April 2019" link="https://ply.gl/umairayub.htmlgenerator" linkText="Google Play"/>
-                        <Project name="ProjectLog" description="Project Progress Logger" type="Android App" date="May 2019" link="https://ply.gl/umairayub.projectlog" linkText="Google Play"/>
-                        <Project name="MaDialog" description="Material Alert Dialog Library" type="Android Library" date="July 2019" link="https://ply.gl/umairayub.madialogdemoapp" linkText="Google Play"/>
-                        <Project name="Rock.it Launcher" description="Simple Launcher and Minimal" type="Android App" date="March 2019" link="https://ply.gl/umairayub.rockitlauncher" linkText="Google Play"/> 
-                        <Project name="Github Stars" description="Open Source Stars" type="Web App" date="Nov 2019" link="https://githubstars.netlify.com" linkText="Visit"/> 
-                        <Project name="FindThatBook" description="Find Books easily by searching title, author, publisher or just a keyword" type="Android App" date="Nov 2019" link="https://ply.gl/codes.umair.findthatbook" linkText="Google Play"/> 
-                        <Project name="PokeDex" description="List of Pokemons with their types and abilities" type="Web App" date="March 2020" link="https://pokidex.netlify.com" linkText="Visit"/>
-                        <Project name="GetMovies" description="A movie Downloading site" type="Web App" date="April 2020" link="https://app-movies.netlify.com" linkText="Visit"/>
-                    </div>
-                </div>
-            </React.Fragment>
-        )
-    }
+  render() {
+    return (
+      <Fragment>
+        <Helmet>
+          <title>{pageTitle} - Umair Ayub</title>
+        </Helmet>
+        <h1>Projects</h1>
+        <div className="projects">
+          <div className="projects_list">
+            <Project
+              name="AppManager"
+              description="Manage all your apps"
+              type="Android App"
+              date="December 2018"
+              link="https://ply.gl/umairayub.appmanager"
+              linkText="Google Play"
+            />
+            <Project
+              name="BitLauncher"
+              description="Minimalist Launcher"
+              type="Android App"
+              date="July 2019"
+              link="https://ply.gl/umairayub.bitlauncher"
+              linkText="Google Play"
+            />
+            <Project
+              name="HTML Generator"
+              description="Generate Html in a Click"
+              type="Android App"
+              date="April 2019"
+              link="https://ply.gl/umairayub.htmlgenerator"
+              linkText="Google Play"
+            />
+            <Project
+              name="ProjectLog"
+              description="Project Progress Logger"
+              type="Android App"
+              date="May 2019"
+              link="https://ply.gl/umairayub.projectlog"
+              linkText="Google Play"
+            />
+            <Project
+              name="MaDialog"
+              description="Material Alert Dialog Library"
+              type="Android Library"
+              date="July 2019"
+              link="https://ply.gl/umairayub.madialogdemoapp"
+              linkText="Google Play"
+            />
+            <Project
+              name="Rock.it Launcher"
+              description="Simple Launcher and Minimal"
+              type="Android App"
+              date="March 2019"
+              link="https://ply.gl/umairayub.rockitlauncher"
+              linkText="Google Play"
+            />
+            <Project
+              name="Github Stars"
+              description="Open Source Stars"
+              type="Web App"
+              date="Nov 2019"
+              link="https://githubstars.netlify.com"
+              linkText="Visit"
+            />
+            <Project
+              name="FindThatBook"
+              description="Find Books easily by searching title, author, publisher or just a keyword"
+              type="Android App"
+              date="Nov 2019"
+              link="https://ply.gl/codes.umair.findthatbook"
+              linkText="Google Play"
+            />
+            <Project
+              name="PokeDex"
+              description="List of Pokemons with their types and abilities"
+              type="Web App"
+              date="March 2020"
+              link="https://pokidex.netlify.com"
+              linkText="Visit"
+            />
+            <Project
+              name="GetMovies"
+              description="A movie Downloading site"
+              type="Web App"
+              date="April 2020"
+              link="https://app-movies.netlify.com"
+              linkText="Visit"
+            />
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
 }
 export default Projects;
