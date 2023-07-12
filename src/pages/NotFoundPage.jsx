55ef5f9122ea03c19a26282d1b5ea65758fb9a97
umairayub79 <umairayub79@gmail.com>
2023-07-12 16:29:07 +0500

    update 404 Page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -6,12 +6,12 @@ export default function NotFound() {
     return (
         <React.Fragment>
             <Helmet>
-                <title>404</title>
+                <title>404 - Page Not Found</title>
             </Helmet>
             <div className='max-w-xl h-full flex flex-col gap-5 justify-center items-center text-center'>
-            <p>The page you are looking for was moved, removed, renamed or might never
-                existed.</p>
-            <Link to="/" className='underline text-blue-700'>Back to the website</Link>
+                <h2 className='font-bold'>Page Not Found</h2>
+                <p>The page you are looking for could not be found. It may have been moved, removed, renamed, or it might never have existed.</p>
+                <Link to="/" className="underline text-blue-700">Go back to the website's homepage</Link>
             </div>
         </React.Fragment>
     )
